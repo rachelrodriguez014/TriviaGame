b516e7ba942825bfa139e8f12c45d4eee157c100
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-04-15 23:04:38 -0700

    updated changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+// set array of questions to be displayed on page...object
+// set up initial start page, when clicked present questions
+// timed slideshow of questions
+// set up variables for start slide, slides shown for correct and incorrect answer, questions slide, 
+
+// 1. In Aladdin, what is the name of Jasmine’s pet tiger? Rajah
+// 2. In The Jungle Book who teaches Mowgli about The Bare Necesseties of life? Baloo
+// 3. Which Disney princess has a raccoon as a sidekick? Pocahantas
+// 4. Which was the first full-length animated movie to be released by Disney? Snow White and the Seven Dwarfs
+// 5. What is the only Disney animated feature film that has a title character who doesn't speak? Dumbo
+// 6. What was the first Disney animated film to feature a completely original storyline? The Lion King
+// 7. In Alice in Wonderland, what is the name of Alice's pet kitten? Dinah
+// 8. In "Princess and the Frog," what fictional country is Prince Naveen from? Maldonia
+
+
+var trivia = [{
+    question: "In Aladdin, what is the name of Aladdin's pet monkey?",
+    answerList: ["Rajah", "Sam", "Aboo", "Iago"],
+    answer: 2
+},{
+    question: "In The Jungle Book, who teaches Mowgli about The Bare Necesseties of Life?",
+    answerList: ["Baloo", "Bagheera", "King Louie", "Sher Khan"],
+    answer: 0
+},{
+    question: "Which Disney princess has a racoon as a sidekick?",
+    answerList: ["Snow White", "Rapunzel", "Sleeping Beauty", "Pocahontas"],
+    answer: 3
+},{
+    question: "Which was the first full-length animated film released by Disney?",
+    answerList: ["Sleeping Beauty", "Snow White and the Seven Dwarfs", "Pocahontas", "The Little Mermaid"],
+    answer: 1
+},{
+    question: "What is the first Disney film to feature a completely original storyline?"
+    answerList: 
+    answer:
+},{
+    question:
+    answerList:
+    answer:
+},{
+    question:
+    answerList:
+    answer:
+},{
+    question:
+    answerList:
+    answer:
+}
+]
