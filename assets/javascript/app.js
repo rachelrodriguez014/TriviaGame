9044db22e5c79760ae85677cd72fd810f33522b3
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-04-16 15:33:48 -0700

    final draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,202 @@
-// set array of questions to be displayed on page...object
-// set up initial start page, when clicked present questions
-// timed slideshow of questions
-// set up variables for start slide, slides shown for correct and incorrect answer, questions slide, 
-
-// 1. In Aladdin, what is the name of Jasmine’s pet tiger? Rajah
-// 2. In The Jungle Book who teaches Mowgli about The Bare Necesseties of life? Baloo
-// 3. Which Disney princess has a raccoon as a sidekick? Pocahantas
-// 4. Which was the first full-length animated movie to be released by Disney? Snow White and the Seven Dwarfs
-// 5. What is the only Disney animated feature film that has a title character who doesn't speak? Dumbo
-// 6. What was the first Disney animated film to feature a completely original storyline? The Lion King
-// 7. In Alice in Wonderland, what is the name of Alice's pet kitten? Dinah
-// 8. In "Princess and the Frog," what fictional country is Prince Naveen from? Maldonia
-
+// declaring trivia questions, choices, photo to display
 
 var trivia = [{
     question: "In Aladdin, what is the name of Aladdin's pet monkey?",
-    answerList: ["Rajah", "Sam", "Aboo", "Iago"],
-    answer: 2
+    choices: ["Rajah", "Sam", "Aboo", "Iago"],
+    answer: 2,
+    image: "assets/images/aboo.png"
 },{
     question: "In The Jungle Book, who teaches Mowgli about The Bare Necesseties of Life?",
-    answerList: ["Baloo", "Bagheera", "King Louie", "Sher Khan"],
-    answer: 0
+    choices: ["Baloo", "Bagheera", "King Louie", "Sher Khan"],
+    answer: 0,
+    image: "assets/images/baloo.jpeg"
 },{
     question: "Which Disney princess has a racoon as a sidekick?",
-    answerList: ["Snow White", "Rapunzel", "Sleeping Beauty", "Pocahontas"],
-    answer: 3
+    choices: ["Snow White", "Rapunzel", "Sleeping Beauty", "Pocahontas"],
+    answer: 3,
+    image: "assets/images/pocahontas.jpg"
 },{
     question: "Which was the first full-length animated film released by Disney?",
-    answerList: ["Sleeping Beauty", "Snow White and the Seven Dwarfs", "Pocahontas", "The Little Mermaid"],
-    answer: 1
+    choices: ["Sleeping Beauty", "Snow White and the Seven Dwarfs", "Pocahontas", "The Little Mermaid"],
+    answer: 1,
+    image: "assets/images/snow-white.png"
 },{
-    question: "What is the first Disney film to feature a completely original storyline?"
-    answerList: 
-    answer:
+    question: "What is the first Disney film to feature a completely original storyline?",
+    choices: ["The Little Mermaid", "Tangled", "The Rescuers", "The Lion King"],
+    answer: 3,
+    image: "assets/images/lion-king.jpg"
 },{
-    question:
-    answerList:
-    answer:
+    question: "In Alice in Wonderland,  what is the name of Alice's pet cat?",
+    choices: ["Mr. Rabbit", " The Mad Hatter", "The Queen of Hearts", "Dinah"],
+    answer: 3,
+    image: "assets/images/alice.png"
 },{
-    question:
-    answerList:
-    answer:
-},{
-    question:
-    answerList:
-    answer:
+    question: "In the Princess and the Frog, what fictional country is Prince Naveen from",
+    choices: ["Genovia", "Maldonia", "France", "Orlinea"],
+    answer: 1,
+    image: "assets/images/tiana.gif"
+}];
+
+// declaring global variables
+
+var correctGuess = 0;
+var wrongGuess = 0;
+var unanswered = 0;
+var timer = 10;
+var intervalId;
+var userGuess = "";
+var running = false;
+var questionCount = trivia.length;
+var index;
+var pick;
+var newArray = [];
+var holder = [];
+
+$("#reset").hide();
+
+// // click button to start game
+
+$("#start").on("click", function() {
+    $("#start").hide();
+    questionDisplay();
+    runTimer();
+    for (var i = 0; i < trivia.length; i++) {
+        holder.push(trivia[i]);
+    }
+})
+
+// // start timer
+
+function runTimer() {
+    if (!running) {
+        intervalId = setInterval(decrement, 1000);
+        running = true;
+    }
 }
-]
+
+// // stop timer
+
+function stopTimer() {
+    running = false;
+    clearInterval(intervalId);
+}
+
+// // creating countdown timer
+
+function decrement() {
+    $("#time-left").html("<h4> Time remaining: " + timer + "</h4>");
+    timer --;
+
+//  stop if at 0
+
+    if (timer === 0) {
+        unanswered++;
+        stopTimer();
+        $("#answerField").html("<p> Time's up!!! The correct answer is: " + pick.choices[pick.answer] + "</p>");
+        hidePicture();
+    }
+}
+
+// // pick a random question from array if not already chosen
+// // display and loop through possible choices
+
+function questionDisplay() {
+    index = Math.floor(Math.random() * trivia.length);
+    pick = trivia[index];
+
+    if (pick.shown) {
+        questionDisplay();
+    }
+    else {
+        console.log(pick.question);
+
+        // go through answer array and display
+
+        $("#questionField").html("<h3>" + pick.question + "</h3>");
+        $("#answerField").html("<h3>" + pick.choices + "<h3>");
+        
+    }
+    $(".answerChoice").on("click", function(){
+        userGuess = parseInt($(this).attr("data-guess"));
+    
+        if(userGuess === pick.answer) {
+        stop()
+        correctGuess++;
+        userGuess = "";
+        $("#answerField").html("<h4>YOU'RE CORRECT!!!</h4>");
+        hidePicture();
+        }
+        else {
+            stopTimer();
+            wrongGuess++;
+            userGuess = "";
+            $("#answerField").html("<h4>Incorrect!! The right answer is:" + pick.choice[pick.answer] + "</h4>")
+            hidePicture();
+        }
+    })
+}
+
+function hidePicture() {
+    $("#answerField").append("<img src=" + pick.image + ">");
+    newArray.push(pick);
+    trivia.splice(index, 1);
+
+    var hiddenPic = setTimeout(function() {
+        $("#answerField").empty();
+        timer = 15;
+
+        if ((wrongGuess + correctGuess + unanswered) === questionCount) {
+            $("#questionField").empty();
+            $("#questionField").html("GAME OVER!! Here's your score:");
+            $("#answerField").append("<h4 Correct Answers: " + correctGuess + "</h4>");
+            $("#answerField").append("<h4 Wrong Answers: " + wrongGuess + "</h4>");
+            $("#answerField").append("<h4 Unanswered: " + unanswered + "</h4>")
+    
+            $("#reset").show();
+            correctGuess = 0;
+            wrongGuess = 0;
+            unanswered = 0;
+        }
+        else {
+            runTimer();
+            questionDisplay();
+        }
+    }, 3000);
+    
+}
+
+$("#reset").on("click", function() {
+    $("#reset").hide();
+    $("#answerField").empty();
+    $("#questionField").empty();
+    for (var i = 0; i < holder.length; i++) {
+        trivia.push(holder[i]);
+    }
+    runTimer();
+    questionDisplay();
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
